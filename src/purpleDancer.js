cc7721ec5e5246b47d38f8e6f77271de5699d9f8
jastra4 <josephstrandmo@gmail.com>
2017-11-27 17:21:02 -0600

    Refactored to pseudoclassical and working.

---
+++ src/purpleDancer.js
@@ -0,0 +1,19 @@
+var makePurpleDancer = function(top, left, timeBetweenSteps) {
+  var purpleDancer = makeDancer(top, left, timeBetweenSteps);
+
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+
+  var oldStep = purpleDancer.step;
+  purpleDancer.$node = $('<span class="purple"></span>');
+  purpleDancer.step = function() {
+    // call the old version of step at the beginning of any call to this new version of step
+    oldStep();
+    // toggle() is a jQuery method to show/hide the <span> tag.
+    // See http://api.jquery.com/category/effects/ for this and
+    // other effects you can use on a jQuery-wrapped html tag.
+    purpleDancer.$node.toggle();
+  };
+
+  return purpleDancer;
+};
